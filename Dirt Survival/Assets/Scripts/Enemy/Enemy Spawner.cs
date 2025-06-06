7954d65913838a256ee2b63685f242708a77f172
Alex_Fontaine <alexmcf8@gmail.com>
2025-06-05 20:25:30 -0400

    Enemy Spawn
    ** Completed **
    - Removed the single spawner and created 3, that spawn an enemy in each lane. (top, middle, bottom)
    - Updated spawner and enemy spawner scripts
    - Created new prefabs of each spawner an enemy that is spawned from each.
    - Adjusted player gravity, and changed the Hitbox colours to red
    
    ** Known Bugs **
    - Issue with the parallax scrolling not being infinite, resulting in the player going out of bounds

--- Dirt Survival/Assets/Scripts/Enemy/Enemy Spawner.cs
+++ Dirt Survival/Assets/Scripts/Enemy/Enemy Spawner.cs
@@ -4,6 +4,9 @@ public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyDown;
     public Transform spawnPoint;
+    private float timer = 0f;
+    private float spawnDelay = 2f;
+    private float RNG;
 
     public void SpawnObject()
     {
@@ -25,12 +28,21 @@ public class EnemySpawner : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        RNG = Random.Range(-3f, 1f);
+        timer = spawnDelay;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // spawns a new enemy every 4 seconds
+        timer -= Time.deltaTime;
+        if (timer <= RNG)
+        {
+            SpawnObject();
+            timer = spawnDelay;
+            RNG = Random.Range(-3f, 1f);
+        }
     }
 }
